ab42d976bd1d5c7f1ddd4d01008c4d1e8f604b55
woodspring <woodspring.toronto@gmail.com>
2021-08-18 23:06:24 -0400

    first commit

---
+++ src/main/java/woodspring/springtzuchitoronto/SpringTzuchiTorontoApplication.java
@@ -0,0 +1,13 @@
+package woodspring.springtzuchitoronto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTzuchiTorontoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTzuchiTorontoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/woodspring/springtzuchitoronto/SpringTzuchiTorontoApplicationTests.java
@@ -0,0 +1,13 @@
+package woodspring.springtzuchitoronto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTzuchiTorontoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
